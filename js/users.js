b5b6d611297c6dbe008c5654fe78192f638b04f9
Corinna <Corinna@Eilis.hsd1.mi.comcast.net.>
2012-04-29 23:15:05 -0400

    Add ability to add new users

--- js/users.js
+++ js/users.js
@@ -7,7 +7,86 @@ tableData.init({
     colspan: 8
 });
 
-Handlebars.registerHelper( 'date', function(dateStr) {
-    var date = new Date(dateStr.substr(5));
-    return (date.getMonth()+ 1) + "/" + date.getDate() ;
+Handlebars.registerHelper('date', function(dateStr) {
+    if (dateStr) {
+        var date = new Date(dateStr.substr(5));
+        if (date.getDate() > 0){
+            return (date.getMonth()+ 1) + "/" + date.getDate();
+        }
+    } 
+    return "";
+});
+
+Handlebars.registerHelper('selected', function(value, option) {
+    if (value && option && value === option) {
+        return "selected"
+    }
+    return "";
+});
+
+Handlebars.registerHelper('integer', function(value) {
+    if (value === "0") {
+        return "";
+    }
+    return value;
+});
+
+manageUser = {
+    init: function(config) {
+        this.config = config;
+        this.config.addLink.on('click', this.showModalWin);
+        this.config.template = Handlebars.compile(this.config.template);
+    },
+    
+    showModalWin: function() {
+        var self = manageUser;
+        var userID = jQuery(this).data('user-id')*1;
+//        jQuery.post('/roster.php', jQuery(this).data('user-id'), function(response) {
+            var template = jQuery('<div></div>').addClass("modal").html(self.config.template({})).css({
+                left: ((window.innerWidth - 800) / 2) - 20
+            }).appendTo('body');
+            
+            jQuery('div.modal_header', template).prepend(
+                jQuery('<div><div>').text('X').attr('id', 'user-close').on('click', function() {
+                    template.remove();
+                })
+            );
+            
+            jQuery('#saveUser', template).on('click', self.saveUser);
+            self.config.form = jQuery('#manageUser', template).on('submit', self.saveUser);
+            self.config.resultsMsgDiv = jQuery('#results_message', template);
+            
+            if (userID === 0) {
+                jQuery('input[name$=password]').remove();
+                jQuery('div.password').remove();
+                jQuery('#user_id').val("0");
+            }
+//        });
+    },
+    
+    saveUser: function(e) {
+        var self = manageUser;
+        e.preventDefault();
+        jQuery.post('/roster.php', self.config.form.serialize(), function(response) {
+            if (!response) {
+                jQuery('input[type!=submit], select', self.config.form).val("");
+                self.config.resultsMsgDiv.text('Save successful');
+                self.config.updateResults.change();
+            } else {
+                if (response === "23000") {
+                    //Duplicate username
+                    self.config.resultsMsgDiv.text('Duplicate username');
+                } else {
+                    self.config.resultsMsgDiv.text('Unknown error: ' + response);
+                }
+            }
+        });
+    }
+};
+
+manageUser.init({
+    template: jQuery('#manage-user-tmpl').html(),
+    addLink: jQuery('#addUser'),
+    modifyLinks: jQuery('.modifyUser'),
+    updateResults: jQuery('.reload')
 });
