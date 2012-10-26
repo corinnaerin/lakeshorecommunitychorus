9076598b70220ea426bebb28da0fa08cedaa6769
Corinna <Corinna@localhost>
2012-10-25 22:46:38 -0400

    convert to angular

--- _partials/links.php
+++
@@ -1,7 +0,0 @@
-<div id="ma-links">
-    <a href="/roster.php">Roster</a>
-    &nbsp;|&nbsp;
-    <a href="/recordings.php">Recordings</a>
-    &nbsp;|&nbsp;
-    <a href="/calendar.php">Calendar</a>
-</div>
