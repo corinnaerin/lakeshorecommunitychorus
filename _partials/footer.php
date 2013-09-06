adb1b6b9a53dacae62acbcebafc2990d60f93bc8
Corinna <Corinna@localhost>
2013-09-06 18:52:31 -0400

    minutes

--- _partials/footer.php
+++ _partials/footer.php
@@ -3,7 +3,7 @@
     <small>
         &copy; 2011 Lakeshore Community Chorus. Design by Corinna Schmidt.<br>
         PO Box 253 Douglas MI 49406<br>
-        269-857-2837
+        616-405-3399
     </small>
 </footer>
 <script src="/js/login.js"></script>
